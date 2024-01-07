08bff048657d20422b9c8d008bc2f08b06eb4383
shantanu <shantanugupta2907@gmail.com>
2024-01-07 14:37:29 +0530

    Add pagination functionality to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
+import PaginationBar from '@/components/PaginationBar';
 import ProductCard from '@/components/ProductCard';
 import prisma from '@/lib/db/prisma';
 import Image from 'next/image';
-
 //it exported on the server
-export default async function Home() {
+
+interface HomeProps {
+  searchParams: { page: string };
+}
+
+export default async function Home({ searchParams: { page = '1' } }: HomeProps) {
+  const currentPage = parseInt(page);
+
+  const Pagesize = 6;
+  const totalItemCount = await prisma.product.count();
+  const totalPages = Math.ceil(totalItemCount / Pagesize);
+
   const products = await prisma.product.findMany({
     orderBy: {
       id: 'desc',
     },
+    skip: (currentPage - 1) * Pagesize,
+    take: Pagesize,
   });
 
   return (
-    <div className="my-4 grid grid-cols-1  md:grid-cols-2 xl:grid-cols-3 gap-4">
-      {products.slice(0).map((product) => (
-        <ProductCard product={product} key={product.id} />
-      ))}
+    <div className="flex flex-col items-center">
+      <div className="my-4 grid grid-cols-1  md:grid-cols-2 xl:grid-cols-3 gap-4">
+        {products.slice(0).map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
+      {totalPages > 1 && <PaginationBar currentPage={currentPage} totalPages={totalPages} />}
     </div>
   );
 }
